223a241c3cf0429711af39898560abcc7394f8d1
n.plachta <n.plachta@microsoft.wsei.edu.pl>
2018-12-06 18:00:13 +0100

    adding files

---
+++ App1/App1/HttpClientPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App1
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class HttpClientPage : ContentPage
+    {
+        private string _url;
+        private string _content;
+
+        public HttpClientPage(string url)
+        {
+            _url = url;
+            InitializeComponent();
+        }
+
+        private async Task Navigate()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = await client.GetAsync(_url);
+                _content = await response.Content.ReadAsStringAsync();
+                await DisplayAlert("Zwrotka", $"Strona zwróciła {response.StatusCode}", "OK");
+            }
+        }
+
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+            await Navigate();
+        }
+    }
+}
---
+++ App1/App1/MainPage.xaml.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace App1
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            lblMain.Text = "To jest text zmieniony po inicjalizacji";
+            lblMain.TextColor = Color.Blue;
+            lblMain.FontSize = 20;
+        }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            var btn = sender as Button;
+            btn.BackgroundColor = Color.Red;
+            lblMain.IsVisible = !lblMain.IsVisible;
+        }
+
+        private async void Button_Clicked_1(object sender, EventArgs e)
+        {
+            await Navigate();
+        }
+
+        private void colorButton(object sender, EventArgs e)
+        {
+            var color = entryColor.Text;
+
+            if (string.IsNullOrWhiteSpace(color))
+            {
+                return;
+            }
+
+            var colorHex = Color.FromHex(color);
+            btnColor.BackgroundColor = colorHex;
+        }
+
+        private void entryColor_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            var isHex = Regex.IsMatch(e.NewTextValue, "^#(([0-9a-fA-F]{2}){3}|([0-9a-fA-F]){3})$");
+            entryColor.BackgroundColor = isHex ?
+                Color.LightSeaGreen :
+            Color.Red;
+        }
+
+        private async void urlEntry_Completed(object sender, EventArgs e)
+        {
+            
+            await Navigate();
+        }
+
+        private async Task Navigate()
+        {
+            var url = urlEntry.Text;
+            if (string.IsNullOrEmpty(url))
+            {
+                await DisplayAlert("Uwaga", "Nie wpisałeś adresu WWW", "Okay");
+                return;
+            }
+            var response = await DisplayAlert("Pytanie", "Czy masz skończone 18 lat, żeby wejść na stronę?", "TAK", "NIE");
+            if (response)
+            {
+                await Navigation.PushAsync(new GridPage());
+            }
+            else
+            {
+                await DisplayAlert("OK", "Wróć jak dorośniesz", "Ok");
+            }
+
+           
+        }
+    }
+}
